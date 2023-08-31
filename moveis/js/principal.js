957a9d9def3bda4e0ee5891bf2c59796689100c5
Rafael Xavier Franco <rafael.franco@loovi.com.br>
2023-08-31 10:22:35 -0300

    Alterando textos do banner.

--- moveis/js/principal.js
+++ moveis/js/principal.js
@@ -2,8 +2,7 @@ var banners =
 ["Os melhores do Brasil!", "Qualidade e preço baixo!"];
 var bannerAtual = 0;
 function trocaBanner() {
-bannerAtual = (bannerAtual + 1) % 2;
-document.querySelector('h2#mensagem').textContent =
+    var banners = ["Do lixo ao luxo!","Reaproveitar é aproveitar!"];document.querySelector('h2#mensagem').textContent =
 banners[bannerAtual];
 }
 setInterval(trocaBanner, 2000);
